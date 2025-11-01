186aa49eebbc1318ba8a27007183ab904b35b0ef
ElishaParker <elishaparker@hotmail.com>
2025-11-01 19:28:19 -0400

    Update wave.js

--- js/modes/wave.js
+++ js/modes/wave.js
@@ -1,1 +1,13 @@
+export function draw(ctx,dataArray,settings){
+  ctx.beginPath();
+  ctx.lineWidth=2;
+  ctx.strokeStyle=`hsl(${settings.hue},${settings.saturation}%,${settings.brightness}%)`;
+  const slice=ctx.canvas.width/dataArray.length;
+  for(let i=0;i<dataArray.length;i++){
+    const y=(dataArray[i]/128)*ctx.canvas.height/2;
+    i===0?ctx.moveTo(0,y):ctx.lineTo(i*slice,y);
+  }
+  ctx.stroke();
+}
+export function updateSetting(){}
 // waveform visual effect placeholder
